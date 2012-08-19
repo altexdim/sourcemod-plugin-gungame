0a7fd5246041cfbe552cd4558161e55913687edc
altex <d.ananyev@gmail.com>
2012-08-19 23:11:55 +0400

    * sdkhooks enabled

--- addons/sourcemod/scripting/compile_gungame_csgo.cmd
+++ addons/sourcemod/scripting/compile_gungame_csgo.cmd
@@ -19,8 +19,8 @@ echo %DATE% %TIME% > %LOG_COMPILE%
 :::csgo version
 
 %DIR_SERVER_SCRIPTING%\spcomp gungame.sp WITH_SDKHOOKS=1 WITH_CSGO_SUPPORT=1    >> %LOG_COMPILE%
-copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SOURCES_PLUGINS%\disabled\gungame_sdkhooks.smx
-copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SERVER_PLUGINS%\disabled\gungame_sdkhooks.smx
+copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SOURCES_PLUGINS%\gungame_sdkhooks.smx
+copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SERVER_PLUGINS%\gungame_sdkhooks.smx
 
 %DIR_SERVER_SCRIPTING%\spcomp gungame.sp WITH_CSGO_SUPPORT=1                >> %LOG_COMPILE%
 %DIR_SERVER_SCRIPTING%\spcomp gungame_afk.sp WITH_CSGO_SUPPORT=1            >> %LOG_COMPILE%
@@ -32,7 +32,7 @@ copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SERVER_PLUGINS%\dis
 %DIR_SERVER_SCRIPTING%\spcomp gungame_tk.sp WITH_CSGO_SUPPORT=1             >> %LOG_COMPILE%
 %DIR_SERVER_SCRIPTING%\spcomp gungame_bot.sp WITH_CSGO_SUPPORT=1            >> %LOG_COMPILE%
 
-copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SOURCES_PLUGINS%\
+copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SOURCES_PLUGINS%\disabled\
 copy %DIR_SERVER_SCRIPTING%\gungame_afk.smx             %DIR_SOURCES_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_config.smx          %DIR_SOURCES_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_display_winner.smx  %DIR_SOURCES_PLUGINS%\
@@ -42,7 +42,7 @@ copy %DIR_SERVER_SCRIPTING%\gungame_stats.smx           %DIR_SOURCES_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_tk.smx              %DIR_SOURCES_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_bot.smx             %DIR_SOURCES_PLUGINS%\
 
-copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SERVER_PLUGINS%\
+copy %DIR_SERVER_SCRIPTING%\gungame.smx                 %DIR_SERVER_PLUGINS%\disabled\
 copy %DIR_SERVER_SCRIPTING%\gungame_afk.smx             %DIR_SERVER_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_config.smx          %DIR_SERVER_PLUGINS%\
 copy %DIR_SERVER_SCRIPTING%\gungame_display_winner.smx  %DIR_SERVER_PLUGINS%\
